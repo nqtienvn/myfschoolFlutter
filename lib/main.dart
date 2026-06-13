df70e13ff9dc489f127ef3bd95337745f6952be9
nqtienvn <quangtiencoder1876@gmail.com>
2026-06-14 01:02:58 +0700

    UI: login and route homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:myfschoolse1913/vn/edu/fpt/apps/mobile/mobile.dart';
+import 'package:myfschoolse1913/loginScreen.dart';
+import 'package:myfschoolse1913/vn/edu/fpt/apps/mobile/theme/fpt_mobile_theme.dart';
 
 void main() {
-  runApp(const MyFSchoolMobileApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: FptMobileTheme.build(),
+      home: const LoginScreen(),
+    );
+  }
 }
